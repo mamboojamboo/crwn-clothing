4ebbb4d01e4ada3be4c9e6e49a885da9786d860e
Mikhail Afonin <afoninmv@gmail.com>
2021-06-22 11:37:04 +0300

    feat(sign-in): add form-input component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -32,23 +34,23 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input 
+                    <FormInput 
                         name="email"
                         type="email"
+                        label="email"
                         value={this.state.email}
+                        handleChange={this.handleChange}
                         required
-                        onChange={this.handleChange}
                     />
-                    <label>Email</label>
 
-                    <input
+                    <FormInput
                         name="password"
                         type="password"
+                        label="password"
                         value={this.state.password}
+                        handleChange={this.handleChange}
                         required
-                        onChange={this.handleChange}
                     />
-                    <label>Password</label>
 
                     <input type="submit" value="Submit Form"/>
                 </form>
