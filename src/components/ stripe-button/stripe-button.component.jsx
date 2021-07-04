86349b948cff0e9904f74d27ad772e588e0f1493
Mikhail Afonin <afoninmv@gmail.com>
2021-07-04 23:22:06 +0300

    feat(checkout): add stripe to app

---
+++ src/components/ stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51J8tUvFooLHDNmtPw0MUwk5WBMPCIbv2p8drQsbxPiLHRdeXDzGx3qPW01SrOiPauwOzKW2r4nWNBF8pWIz7rrAm00Ino51XLy';
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+};
+
+export default StripeCheckoutButton;
