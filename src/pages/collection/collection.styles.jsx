6e72f481c926518b867a753d6a477fe8ee4ba731
Mikhail Afonin <afoninmv@gmail.com>
2021-07-05 15:46:39 +0300

    feat(pages/collection): change scss styles to styled-components

---
+++ src/pages/collection/collection.styles.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const CollectionPageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const TitleContainer = styled.h2`
+    font-size: 38px;
+    margin: 0 auto 30px;
+`;
+
+const ItemsContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 10px;
+
+    & > div {
+        margin-bottom: 30px;
+      }
+`;
+
+export {
+    CollectionPageContainer,
+    TitleContainer,
+    ItemsContainer
+};
