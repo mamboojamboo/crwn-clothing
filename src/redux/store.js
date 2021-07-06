ace7d826a696ce45502685c4ffd440bbbdb32573
Mikhail Afonin <afoninmv@gmail.com>
2021-07-07 00:42:00 +0300

    feat(redux/store): check heroku process.env.NODE_ENV

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,11 +8,12 @@ import rootReducer from "./root-reducer";
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
+    console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
     middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+console.log('middlewares: ', middlewares.length)
 
-const persistor = persistStore(store);
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export { store, persistor };
+export const persistor = persistStore(store);
