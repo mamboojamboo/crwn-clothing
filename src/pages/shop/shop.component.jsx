df5973634d96c96d497b4a66d35f18a263c3e4e0
Mikhail Afonin <afoninmv@gmail.com>
2021-07-09 20:34:43 +0300

    feat(shop.saga): add fetchCollectionsAsync

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,13 +5,13 @@ import { connect } from 'react-redux';
 import CollectionsOverviewContainer from '../../components/collections-overview/collection-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 class ShopPage extends React.Component {
 
     componentDidMount() {
-        const { fetchCollectionsStartAsync } = this.props;
-        fetchCollectionsStartAsync()
+        const { fetchCollectionsStart } = this.props;
+        fetchCollectionsStart()
     }
     render() {
         const { match } = this.props;
@@ -26,7 +26,7 @@ class ShopPage extends React.Component {
 };
 
 const mapDispatchToProps = dispath => ({
-    fetchCollectionsStartAsync: () => dispath(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispath(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage);
