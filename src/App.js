015996e6382aad65adb564c38f9563693bb5fc11
Mikhail Afonin <afoninmv@gmail.com>
2021-07-10 11:12:41 +0300

    feat(redux/user): add user.sagas and implement saga in sign-in

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
@@ -20,26 +19,6 @@ import './App.css';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { setCurrentUser } = this.props;
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
-          })
-        })
-      } else {
-        setCurrentUser(userAuth)
-      }
-
-    })
-  }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
