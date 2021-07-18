cedc5f9735e0d42d238540e915e76d0954d42084
Mikhail Afonin <afoninmv@gmail.com>
2021-07-18 15:53:21 +0300

    feat(client/stripe-button): add axios to onToken

--- client/src/components/ stripe-button/stripe-button.component.jsx
+++ client/src/components/ stripe-button/stripe-button.component.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51J8tUvFooLHDNmtPw0MUwk5WBMPCIbv2p8drQsbxPiLHRdeXDzGx3qPW01SrOiPauwOzKW2r4nWNBF8pWIz7rrAm00Ino51XLy';
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response =>  {
+            alert('Payment successfull')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error))
+            alert(
+                'There was an issue wis your payment. Please sure you use the provided credit card.'
+            )
+        })
     }
 
     return (
