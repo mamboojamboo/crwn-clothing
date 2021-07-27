b1995750710bd05d1ecdc18f01f1058dfd11a166
Mikhail Afonin <afoninmv@gmail.com>
2021-07-27 17:10:11 +0300

    feat(server): add compression

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,15 +11,16 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors());
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get('*', function(req, res) {
+    app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     });
 };
